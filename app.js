753e9941ab0380511fe1b5c55ea1d6de22d4c08f
JakobyM117 <156844634+JakobyM117@users.noreply.github.com>
2026-03-20 12:52:45 -0500

    Changes

--- app.js
+++ app.js
@@ -1,71 +1,116 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
 
-function ArtItem(title, price, type) {
-  this.title = title;
-  this.price = price;
-  this.type = type;
+  //Start the Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
 
-  // Object method
-  this.getInfo = function() {
-    return `${this.title} (${this.type}) - $${this.price.toFixed(2)}`;
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
   };
-}
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
 
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
 
-// Array of cart items
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
 
-const cart = [
-  new ArtItem("Canvas Sunset", 45.99, "Canvas"),
-  new ArtItem("Digital Sketch", 19.99, "Digital"),
-  new ArtItem("Framed Poster", 27.50, "Poster"),
-  new ArtItem("Limited Edition Print", 125.00, "Print")
-];
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
 
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
 
-// ARRAY METHOD
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
 
-function addToCart(item) {
-  cart.push(item);
-}
+  //Is call all the inner function
+  startGame();
+  playMatch();
+};
 
-// Add a new item
-addToCart(new ArtItem("Music Art Print", 59.95, "Print"));
-
-
-function showCart() {
-  const outputDiv = document.getElementById("cartOutput");
-  outputDiv.innerHTML = "<h3>Cart Items:</h3>";
-
-
-  cart.forEach(function(item, index) {
-    outputDiv.innerHTML += `<p>${index+1}. ${item.getInfo()}</p>`;
-  });
-
-  const total = cart.reduce(function(sum, item) {
-    return sum + item.price;
-  }, 0);
-
-  outputDiv.innerHTML += `<p><strong>Total:</strong> $${total.toFixed(2)}</p>`;
-}
-
-
-// FILTER EXPENSIVE ITEMS
-
-function showExpensiveItems() {
-  const expensive = cart.filter(function(item) {
-    return item.price > 50;
-  });
-
-  const outputDiv = document.getElementById("cartOutput");
-  outputDiv.innerHTML = "<h3>Items over $50:</h3>";
-
-  expensive.forEach(function(item) {
-    outputDiv.innerHTML += `<p>${item.getInfo()}</p>`;
-  });
-}
-
-
-document.getElementById("showCartBtn")
-  .addEventListener("click", showCart);
-
-document.getElementById("filterExpensiveBtn")
-  .addEventListener("click", showExpensiveItems);
+//start the game function
+game();
